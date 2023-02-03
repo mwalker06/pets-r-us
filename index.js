49cbf4a1daa98f2ab8d8149cb66d0b90bfb702aa
Megan Walker <mwalker@TEC0661DEAPP20.local>
2023-02-03 10:33:31 -0600

    created groomin and boarding pages, updated index, moved headder and footer to partials

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // Tells Express to use the public folder for static assets.
-app.use('/styles',express.static(path.join(__dirname, 'public/styles')));
-app.use('/images',express.static(path.join(__dirname, 'public/images')));
+app.use('/styles', express.static(path.join(__dirname, 'public/styles')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 // Routes
 app.get("/", (req, res) => {
@@ -33,7 +33,12 @@ app.get("/", (req, res) => {
 app.get("/grooming", (req, res) => {
   res.render("grooming.ejs");
 });
-
+app.get("/boarding", (req, res) => {
+  res.render("boarding.ejs");
+});
+app.get("/training", (req, res) => {
+  res.render("training.ejs");
+});
 // Tells Express to listen on port 3000
 const PORT = process.env.PORT || 3000;
 
